ffdce7aa26ee19f55a6e13a6dfb4e0c806c422e7
yhosok <hosokawa@openlogi.com>
2019-06-03 08:55:08 +0900

    for japanese

--- pytorch_pretrained_bert/tokenization.py
+++ pytorch_pretrained_bert/tokenization.py
@@ -221,7 +221,9 @@ class BasicTokenizer(object):
         # and generally don't have any Chinese data in them (there are Chinese
         # characters in the vocabulary because Wikipedia does have some Chinese
         # words in the English Wikipedia.).
-        text = self._tokenize_chinese_chars(text)
+        
+        # for japanese
+        # text = self._tokenize_chinese_chars(text)
         orig_tokens = whitespace_tokenize(text)
         split_tokens = []
         for token in orig_tokens:
